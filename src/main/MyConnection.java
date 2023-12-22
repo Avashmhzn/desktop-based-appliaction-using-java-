b7805013cd78792b5804a76899a789f5adc08361
Avashmhzn <devilmhzn@gmail.com>
2023-12-22 09:59:10 +0545

    java commit

---
+++ src/main/MyConnection.java
@@ -0,0 +1,15 @@
+package main;
+import java.sql.*;
+
+public class MyConnection {
+    public static Connection getConnection(){
+        Connection con = null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/emsdb","root","");
+        } catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return con;
+    }
+}
